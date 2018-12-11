a2deb41e161b321d75bcb7b3d10826f4a599c96d
HansR <hans.roelofsen@wur.nl>
2018-12-11 17:26:51 +0100

    a en b vars toegevoegd

--- test01.py
+++ test01.py
@@ -5,3 +5,6 @@ print ('hello Johan')
 a = [i for i in range(0, 100)]
 b = [j for j in range(100, 200)]
 print ('nogmaals gewijzigd om 1701uur!')
+
+a = 'hallo Hans, nog een wijziging om 1724 uur'
+b = 'ik heb honger ondertussen!'
